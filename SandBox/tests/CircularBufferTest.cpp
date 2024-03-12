b406e028fd20150807e9e7550fe9111b849899bc
René Moll <github@r-moll.nl>
2024-03-12 12:43:11 +0100

    sandbox: add initial buffer notes

---
+++ SandBox/tests/CircularBufferTest.cpp
@@ -0,0 +1,10 @@
+/*
+* Create a new buffer
+* Destroy an existing buffer
+* New buffer is empty
+* New buffer is not full
+* Put a number of items into the buffer
+* Get a number of items out from the buffer
+* Fill the buffer until full
+* Fill the buffer with more data than capacity
+*/
