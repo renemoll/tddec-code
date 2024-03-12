5bdae3c3d1fa2ec012baa8f9510d661dc66cf223
René Moll <github@r-moll.nl>
2024-03-12 17:15:35 +0100

    sandbox: first implementation of LedDriver

--- SandBox/src/LedDriver.c
+++ SandBox/src/LedDriver.c
@@ -1,39 +1,22 @@
-/***
- * Excerpted from "Test-Driven Development for Embedded C",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/jgade for more book information.
-***/
-/*- ------------------------------------------------------------------ -*/
-/*-    Copyright (c) James W. Grenning -- All Rights Reserved          -*/
-/*-    For use by owners of Test-Driven Development for Embedded C,    -*/
-/*-    and attendees of Renaissance Software Consulting, Co. training  -*/
-/*-    classes.                                                        -*/
-/*-                                                                    -*/
-/*-    Available at http://pragprog.com/titles/jgade/                  -*/
-/*-        ISBN 1-934356-62-X, ISBN13 978-1-934356-62-3                -*/
-/*-                                                                    -*/
-/*-    Authorized users may use this source code in your own           -*/
-/*-    projects, however the source code may not be used to            -*/
-/*-    create training material, courses, books, articles, and         -*/
-/*-    the like. We make no guarantees that this source code is        -*/
-/*-    fit for any purpose.                                            -*/
-/*-                                                                    -*/
-/*-    www.renaissancesoftware.net james@renaissancesoftware.net       -*/
-/*- ------------------------------------------------------------------ -*/
 
 #include "LedDriver.h"
-#include <stdlib.h>
-#include <memory.h>
+// #include <stdlib.h>
+// #include <memory.h>
 
-void LedDriver_Create(void)
+static uint16_t* g_ledState = NULL;
+
+void LedDriver_Create(uint16_t* ledState)
 {
+	g_ledState = ledState;
+	*g_ledState = 0;
 }
 
-void LedDriver_Destroy(void)
+void LedDriver_TurnOn(uint16_t ledId)
 {
+	*g_ledState |= (1 << (ledId - 1));
 }
 
-
+void LedDriver_TurnOff(uint16_t ledId)
+{
+	*g_ledState &= ~(1 << (ledId - 1));
+}
--- SandBox/tests/LedDriverTest.cpp
+++ SandBox/tests/LedDriverTest.cpp
@@ -1,20 +1,3 @@
-//- ------------------------------------------------------------------
-//-    Copyright (c) James W. Grenning -- All Rights Reserved         
-//-    For use by owners of Test-Driven Development for Embedded C,   
-//-    and attendees of Renaissance Software Consulting, Co. training 
-//-    classes.                                                       
-//-                                                                   
-//-    Available at http://pragprog.com/titles/jgade/                 
-//-        ISBN 1-934356-62-X, ISBN13 978-1-934356-62-3               
-//-                                                                   
-//-    Authorized users may use this source code in your own          
-//-    projects, however the source code may not be used to           
-//-    create training material, courses, books, articles, and        
-//-    the like. We make no guarantees that this source code is       
-//-    fit for any purpose.                                           
-//-                                                                   
-//-    www.renaissancesoftware.net james@renaissancesoftware.net      
-//- ------------------------------------------------------------------
 
 extern "C"
 {
@@ -25,19 +8,116 @@ extern "C"
 
 TEST_GROUP(LedDriver)
 {
+    uint16_t ledState = 0;
+
     void setup()
     {
-      LedDriver_Create();
-    }
-
-    void teardown()
-    {
-       LedDriver_Destroy();
+      LedDriver_Create(&ledState);
     }
 };
 
-TEST(LedDriver, Create)
+TEST(LedDriver, Initialization)
 {
-//  FAIL("Start here");
+  // Prepare
+  uint16_t ledStateLocal = 0xA5A5;
+
+  // Execute
+  LedDriver_Create(&ledStateLocal);
+
+  // Verify
+  CHECK_EQUAL(ledStateLocal, 0);
 }
 
+TEST(LedDriver, TurnOnLedOne)
+{
+  // Prepare
+
+  // Execute
+  LedDriver_TurnOn(1);
+
+  // Verify
+  CHECK_EQUAL(ledState, 0x0001);
+}
+
+TEST(LedDriver, TurnOffLedOne)
+{
+  // Prepare
+  LedDriver_TurnOn(1);
+
+  // Execute
+  LedDriver_TurnOff(1);
+
+  // Verify
+  CHECK_EQUAL(ledState, 0x0000);
+}
+
+TEST(LedDriver, TurnOnMultipleLeds)
+{
+  // Prepare
+
+  // Execute
+  LedDriver_TurnOn(1);
+  LedDriver_TurnOn(8);
+  LedDriver_TurnOn(9);
+
+  // Verify
+  CHECK_EQUAL(ledState, 0x0181);
+}
+
+TEST(LedDriver, TurnOffMultipleLeds)
+{
+  // Prepare
+  LedDriver_TurnOn(2);
+  LedDriver_TurnOn(8);
+  LedDriver_TurnOn(9);
+
+  // Execute
+  LedDriver_TurnOff(8);
+  LedDriver_TurnOff(9);
+
+  // Verify
+  CHECK_EQUAL(ledState, 0x0002);
+}
+
+TEST(LedDriver, TurnOnAllLeds)
+{
+  // Prepare
+
+  // Execute
+  for (int i = 0; i < 16; ++i) {
+    LedDriver_TurnOn(i + 1);  // note: LED ID starts at 1
+  }
+
+  // Verify
+  CHECK_EQUAL(ledState, 0xFFFF);
+}
+
+TEST(LedDriver, TurnOffAllLeds)
+{
+  // Prepare
+  for (int i = 0; i < 16; ++i) {
+    LedDriver_TurnOn(i + 1);
+  }
+
+  // Execute
+  for (int i = 0; i < 16; ++i) {
+    LedDriver_TurnOff(i + 1);
+  }
+
+  // Verify
+  CHECK_EQUAL(ledState, 0x0000);
+}
+
+TEST(LedDriver, TurningOnUnsupportedLedHasNoEffect)
+{
+  // Prepare
+  LedDriver_TurnOn(2);
+  LedDriver_TurnOn(8);
+  LedDriver_TurnOn(9);
+
+  // Execute
+  LedDriver_TurnOn(17);
+
+  // Verify
+  CHECK_EQUAL(ledState, 0x0182);
+}
